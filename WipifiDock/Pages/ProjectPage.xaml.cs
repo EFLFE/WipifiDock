b0fae9f9138ad367027a2f5c45be4af55f43061b
Igor <0eflfe0@gmail.com>
2016-09-22 10:19:33 +0300

    fix toolbox

--- WipifiDock/Pages/ProjectPage.xaml.cs
+++ WipifiDock/Pages/ProjectPage.xaml.cs
@@ -1,22 +1,82 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace WipifiDock.Pages
 {
-    public partial class ProjectPage : Page
+    public sealed partial class ProjectPage : Page
     {
         public ProjectPage()
         {
             InitializeComponent();
         }
 
-        private void Page_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        // on load (navigate to this page)
+        private void Page_Loaded(object sender, RoutedEventArgs e)
         {
         }
 
+        // когда выбрана вкладка
         private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
         }
+
+        #region Click Events
+
+        private void configProjectButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void openProjectFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void projectToWebButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void renderButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void mdButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void htmlButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void addDockButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void addFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void deleteDockButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void deleteFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void collapseButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void expandButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void renameButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        #endregion
+
     }
 }
